78083239998b126fc369172d73d76c1367d65ff4
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-19 16:11:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,7 @@ class Application
       else
         resp.write "Could not find this item"
         resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
