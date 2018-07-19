dc37476fa75040fdcee23f00788c1c70edd99381
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-19 16:07:10 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+
+      resp.write item.price
+    end
+
+    resp.finish
+  end
+
+end
